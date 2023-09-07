5aca3ac5d3b594331994bdfdcb886d0c8a538bf8
DmitrySergienko <dmitrisergienko@gmail.com>
2023-09-07 14:33:53 +0300

    bottom navigation bar

--- lib/presentation/screens/tabs_screen.dart
+++ lib/presentation/screens/tabs_screen.dart
@@ -8,10 +8,47 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreen extends State<TabsScreen> {
+  //for bottom navigation tab
+  int _selectedTabIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('test'),
+      appBar: AppBar(),
+      body: const Text('test'),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: _selectPage,
+          currentIndex: _selectedTabIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home), // A built-in Flutter icon
+              label: 'Главная',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shop), // A built-in Flutter icon
+              label: 'Покупки',
+            ),
+            BottomNavigationBarItem(
+              icon:
+                  Icon(Icons.account_balance_wallet), // A built-in Flutter icon
+              label: 'Платежи',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.textsms_rounded), // A built-in Flutter icon
+              label: 'Чат',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.redeem_rounded), // A built-in Flutter icon
+              label: 'Бонусы',
+            ),
+          ]),
     );
   }
 }
